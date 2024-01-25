b9fe553d16d81b1ccdffee549c031bb81c7710ea
trichimtrich <trichimtrich@gmail.com>
2024-01-25 10:51:54 +0800

    add json indent

--- src/main.ts
+++ src/main.ts
@@ -141,7 +141,7 @@ export class Main implements BookmarkDataProvider, BookmarManager {
             obj[this.savedActiveGroupKey] = this.activeGroup.name;
             obj[this.savedHideInactiveGroupsKey] = this.hideInactiveGroups;
             obj[this.savedHideAllKey] = this.hideAll;
-            let json = JSON.stringify(obj);
+            let json = JSON.stringify(obj, null, 4);
             // get directory path from savedBookmarksFilePath
             fs.mkdirSync(this.savedBookmarksFilePath.substring(0, this.savedBookmarksFilePath.lastIndexOf("/")), { recursive: true });
             fs.writeFileSync(this.savedBookmarksFilePath, json);
@@ -1511,7 +1511,7 @@ export class Main implements BookmarkDataProvider, BookmarManager {
     private restoreSavedState() {
         // check if labeled file bookmarks are present
         if (this.savedBookmarksFilePath) {
-            vscode.window.showInformationMessage(this.savedBookmarksFilePath);
+            // vscode.window.showInformationMessage(this.savedBookmarksFilePath);
             if (fs.existsSync(this.savedBookmarksFilePath)) {
                 let data = fs.readFileSync(this.savedBookmarksFilePath, 'utf8');
                 let obj = JSON.parse(data);
